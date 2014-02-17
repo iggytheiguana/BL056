84bbe66b43b8b155e760a97306bfb90366bbe9d7
Bobby Gill <bobby.gill@gmail.com>
2014-02-17 12:29:31 -0500

    cleaning up old source files

--- QCodeMe-Android/qodeme/src/main/java/biz/softtechnics/qodeme/ui/common/ExtendedAdapterBasedView.java
+++
@@ -1,8 +0,0 @@
-package biz.softtechnics.qodeme.ui.common;
-
-/**
- * Created by Alex on 10/24/13.
- */
-public interface ExtendedAdapterBasedView<T, C extends ExAdapterCallback> extends AdapterBasedView<T> {
-    void fill(T t, C c, int position, T previousElement);
-}
--- QCodeMe-Android/qodeme/src/main/java/biz/softtechnics/qodeme/ui/contacts/ContactListAdapterCallback.java
+++
@@ -1,9 +0,0 @@
-package biz.softtechnics.qodeme.ui.contacts;
-
-import biz.softtechnics.qodeme.ui.common.ExAdapterCallback;
-
-/**
- * Created by Alex on 11/6/13.
- */
-public interface ContactListAdapterCallback extends ExAdapterCallback{
-}
