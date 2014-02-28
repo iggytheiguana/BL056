9d9cc7b6d10d8fddb3fbcd08affc0ec8e68d31d6
Bobby Gill <bobby.gill@gmail.com>
2014-02-28 17:44:42 -0500

    modified hello world to be a web socket

--- QCodeMe-Server/qodeme/tornado/helloworld.py
+++ QCodeMe-Server/qodeme/tornado/helloworld.py
@@ -1,14 +1,24 @@
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
+import tornado.httpserver
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print 'new connection'
+        self.write_message("Hello World")
+      
+    def on_message(self, message):
+        print 'message received %s' % message
+ 
+    def on_close(self):
+      print 'connection closed'
 
 application = tornado.web.Application([
-    (r"/", MainHandler),
+    (r"/", WSHandler),
 ])
 
 if __name__ == "__main__":
-    application.listen(8888)
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
