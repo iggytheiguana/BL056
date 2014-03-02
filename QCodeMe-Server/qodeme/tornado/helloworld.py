7244b5b15d9bcd407934371bc618fffb4096915a
Bobby Gill <bobby.gill@gmail.com>
2014-03-02 11:43:31 -0500

    added logging to python code

--- QCodeMe-Server/qodeme/tornado/helloworld.py
+++ QCodeMe-Server/qodeme/tornado/helloworld.py
@@ -7,12 +7,15 @@ import logging
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print 'new connection'
+        logger.debug("new connection")
         self.write_message("Hello World")
       
     def on_message(self, message):
+    	logger.debug("message received %s" % message)
         print 'message received %s' % message
  
     def on_close(self):
+      logger.debug("connection closed")
       print 'connection closed'
 
 logger = logging.getLogger(__name__)
@@ -21,7 +24,7 @@ logger.setLevel(logging.DEBUG)
 application = tornado.web.Application([
     (r"/", WSHandler),
 ])
-
+logger.info("Tornado server starting...")
 if __name__ == "__main__":
 
     http_server = tornado.httpserver.HTTPServer(application)
