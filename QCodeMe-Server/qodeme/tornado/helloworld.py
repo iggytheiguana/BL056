ec8c700f74112c52337514462be9144aee2069a9
Bobby Gill <bobby.gill@gmail.com>
2014-03-02 12:35:59 -0500

    added logging to python code

--- QCodeMe-Server/qodeme/tornado/helloworld.py
+++ QCodeMe-Server/qodeme/tornado/helloworld.py
@@ -3,6 +3,15 @@ import tornado.web
 import tornado.websocket
 import tornado.httpserver
 import logging
+import torndb
+import tornado.options
+
+from tornado.options import define, options
+
+define("mysql_host", default="127.0.0.1:3306", help="")
+define("mysql_database", default="qrchat", help="")
+define("mysql_user", default="root", help="")
+define("mysql_password", default="aspqwe", help="")
 
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
@@ -17,6 +26,8 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     def on_close(self):
       logger.debug("connection closed")
       print 'connection closed'
+
+
 logger.info("Setting up logger...")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -24,6 +35,13 @@ logger.setLevel(logging.DEBUG)
 application = tornado.web.Application([
     (r"/", WSHandler),
 ])
+
+# Have one global connection to the blog DB across all handlers
+self.db = torndb.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password)
+
+
 logger.info("Tornado server starting...")
 if __name__ == "__main__":
 
